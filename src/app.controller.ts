67f6d16c85699edde1663320407666abb92a523f
pitlei <rushwender@gmail.com>
2021-04-04 19:50:07 +0800

    initial commit

---
+++ src/app.controller.ts
@@ -0,0 +1,19 @@
+import { Controller, Get, Post, Body } from '@nestjs/common';
+import { AppService } from './app.service';
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get()
+  getHello(): string {
+    return this.appService.getHello();
+  }
+
+  @Post()
+  postSubmit(@Body() test): any{
+    console.log(test);
+  }
+  
+}
+ 
---
+++ src/app.service.ts
@@ -0,0 +1,8 @@
+import { Injectable } from '@nestjs/common';
+import { Connection } from 'mongoose';
+@Injectable()
+export class AppService {
+  getHello(): string {
+    return 'Hello World!';
+  }
+}
